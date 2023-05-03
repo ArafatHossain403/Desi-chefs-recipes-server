97dcb4fd2abf8c4ff1a353803a9610bb90b7b516
Arafat Hossain <arafatnubt@gmail.com>
2023-05-03 20:15:14 +0600

    json file make and index file make

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app = express();
+const cors = require('cors');
+const port= process.env.PORT || 5000;
+
+
+const chefs = require('./data/chefs.json'); 
+
+app.use(cors());
+
+app.get('/', (req, res) =>{
+         res.send("chef is running")
+});
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+});
+
+
+app.listen(port, () => {
+    console.log(`chef port is running: ${port}`)
+})
